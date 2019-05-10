c357d2255b41f07fea5c126134aaea3acc415e14
wasupandceacar <wasupandceacar@gmail.com>
2019-05-10 20:34:51 +0900

    添加log

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"danser/configui"
+	"log"
+	"os"
 )
 
-
+var IDEDEBUG = false
 
 func main() {
+	if !IDEDEBUG {
+		// 设置log文件
+		file, _ := os.OpenFile("danser.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		defer file.Close()
+		log.SetOutput(file)
+		log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime)
+	}
+
 	configui.UImain()
 }
